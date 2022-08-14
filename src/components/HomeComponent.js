69476a879ba9a940985fda996c32046d27d8388f
sabeensstha <sabeensstha@gmail.com>
2022-08-14 12:47:31 +0545

    Single Page Application 1

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from "react";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardSubtitle,
+  CardTitle,
+} from "reactstrap";
 
-const Home = () => {
+const RenderCard = ({item}) => {
   return (
-    <div className='container'>
-        <h4>Home</h4>
+    <Card>
+      <CardImg src={item.image} alt={item.name} />
+      <CardBody>
+        <CardTitle>{item.name}</CardTitle>
+        {item.designation ? (
+          <CardSubtitle>{item.designation}</CardSubtitle>
+        ) : null}
+        <CardText>{item.description}</CardText>
+      </CardBody>
+    </Card>
+  );
+};
+
+const Home = (props) => {
+  return (
+    <div className="container">
+      <div className="row align-items-start">
+        <div className="col-12 col-md m-1">
+          <RenderCard item={props.dishes} />
+        </div>
+        <div className="col-12 col-md m-1">
+          <RenderCard item={props.promotions} />
+        </div>
+        <div className="col-12 col-md m-1">
+          <RenderCard item={props.leaders} />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,13 +2,20 @@ import React, { useState } from "react";
 import Home from "./HomeComponent";
 import Menu from "./Menu";
 import DishDetail from "./DishDetail";
-import { DISHES } from "../shared/dishes";
+import Contact from "./Contact";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
+import { DISHES } from "../shared/dishes";
+import { COMMENTS } from "../shared/comments";
+import { PROMOTIONS } from "../shared/promotions";
+import { LEADERS } from "../shared/leaders";
 import { Routes, Route } from "react-router-dom";
 
 const Main = () => {
   const [dishes, setdishes] = useState(DISHES);
+  const [comments, setComments] = useState(COMMENTS);
+  const [leaders, setLeaders] = useState(LEADERS);
+  const [promotions, setPromotions] = useState(PROMOTIONS);
   const [selectedDish, setSelectedDish] = useState(null);
   function onDishSelect(dishId) {
     setSelectedDish(dishId);
@@ -20,11 +27,16 @@ const Main = () => {
       );
     else return <div></div>;
   };
+  
   return (
     <div>
       <Header />
       <Routes>
-        <Route path="/home" element={<Home />} />
+        <Route path="/home" element={<Home dishes={dishes.filter((dish) => dish.featured)[0]}
+        promotions={promotions.filter((promo) => promo.featured)[0]}       
+        leaders={leaders.filter((leader) => leader.featured)[0]}       
+        />} />
+        <Route exact path="/contact" element={<Contact />} />
         <Route exact path="/menu" element={<Menu dishes={dishes} />} />
         <Route path="*" element={<Home />} />
       </Routes>
